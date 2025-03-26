3177b40c90720822ef7a7b56688ad7c3de057984
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 18:31:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
