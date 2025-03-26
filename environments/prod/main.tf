ef18c25b35216542482329fafe9e3e2dbf63b2a1
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 18:07:50 +0530

    Create main.tf

---
+++ environments/prod/main.tf
@@ -0,0 +1,33 @@
+# environments/prod/main.tf
+module "hub_vpc" {
+  source             = "../../modules/vpc"
+  name               = "hub"
+  environment        = "prod"
+  vpc_cidr           = "10.2.0.0/16"
+  public_subnet_cidrs = ["10.2.1.0/24", "10.2.2.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
+}
+
+module "spoke_vpc" {
+  source             = "../../modules/vpc"
+  name               = "spoke"
+  environment        = "prod"
+  vpc_cidr           = "10.3.0.0/16"
+  public_subnet_cidrs = ["10.3.1.0/24", "10.3.2.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
+}
+
+module "shared_services" {
+  source      = "../../modules/shared-services"
+  vpc_id      = module.hub_vpc.vpc_id
+  environment = "prod"
+  vpc_cidr    = "10.2.0.0/16"
+}
+
+module "vpc_peering" {
+  source       = "../../modules/vpc-peering"
+  hub_vpc_id   = module.hub_vpc.vpc_id
+  spoke_vpc_id = module.spoke_vpc.vpc_id
+  hub_name     = "hub"
+  spoke_name   = "spoke"
+}
