1bb884a77bf5522111f71b7bc7ff351172d1cc90
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 20:39:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,11 +9,4 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>3.0"
-    }
-  }
 }
