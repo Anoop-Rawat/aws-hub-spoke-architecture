457333433708fb427f5063882c6ad6ae00c668c0
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 18:41:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "state/terraform.tfstate"
+    bucket         = "your-terraform-state-bucket135"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
